9e7eba603e2e635e3d52efa2b7bc4b9db29d1049
bharat <sutharbharat10001@gmail.com>
2026-02-13 08:46:49 +0530

    model classes for serialization and deserialization

---
+++ lib/features/search/data/models/search_result_model.dart
@@ -0,0 +1,5 @@
+import 'package:pinterest_clone/features/search/domain/entities/search_result_entity.dart';
+
+class SearchResultModel extends SearchResultEntity {
+  const SearchResultModel({required super.item});
+}
